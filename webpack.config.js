256695e95cd904be21d319b967f317e55ce61cc3
Matt Potts <matt@@mrpotts.uk>
2021-01-27 16:49:42 +0000

    feat(webpack): Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/scripts/module.ts'),
+    },
+    output: {
+        path: path.resolve(__dirname, '/dist/scripts'),
+        filename: 'module.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+            }
+        ]
+    }
+};
