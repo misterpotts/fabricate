89d603bf128c3bc17d4280168f7d2a438c8306ee
Matt Potts <matt@@mrpotts.uk>
2022-09-06 19:00:45 +0100

    - adds system duplication and deletion

--- src/scripts/interface/apps/CraftingSystemManagerApp.ts
+++ src/scripts/interface/apps/CraftingSystemManagerApp.ts
@@ -81,7 +81,12 @@ class CraftingSystemManagerApp extends FormApplication {
         console.log("Save state");
     }
 
+    _restoreState() {
+        console.log("Restore state");
+    }
+
     protected _contextMenu(html: JQuery) {
+        const craftingSystemManagerApp = this;
         new ContextMenu(html, ".fabricate-crafting-system", [
             {
                 name: `${Properties.module.id}.CraftingSystemManagerApp.contextMenu.export`,
@@ -93,15 +98,58 @@ class CraftingSystemManagerApp extends FormApplication {
             {
                 name: `${Properties.module.id}.CraftingSystemManagerApp.contextMenu.delete`,
                 icon: `<i class="fa-solid fa-trash"></i>`,
+                condition: (element: JQuery) => {
+                    const systemId = element.data()["systemId"] as string;
+                    const GAME = new GameProvider().globalGameObject();
+                    const craftingSystems = GAME.settings.get(Properties.module.id, Properties.settings.craftingSystems.key) as CraftingSystemDefinition[];
+                    const systemToDelete = craftingSystems.find(system => system.id === systemId);
+                    return systemToDelete && !systemToDelete.locked;
+                },
                 callback: async (element: JQuery) => {
-                    console.log(element.data()["systemId"]);
+                    const systemId = element.data()["systemId"] as string;
+                    if (!systemId) {
+                        console.error("Cannot delete system: no ID was provided. ");
+                        return;
+                    }
+                    const GAME = new GameProvider().globalGameObject();
+                    const craftingSystems = GAME.settings.get(Properties.module.id, Properties.settings.craftingSystems.key) as CraftingSystemDefinition[];
+                    const systemToDelete = craftingSystems.find(system => system.id === systemId);
+                    if (!systemId) {
+                        console.error(`Could not find system ${systemId}`);
+                        return;
+                    }
+                    Dialog.confirm({
+                        title: GAME.i18n.localize(`${Properties.module.id}.CraftingSystemManagerApp.deleteSystemConfirm.title`),
+                        content: `<p>${GAME.i18n.format(Properties.module.id + ".CraftingSystemManagerApp.deleteSystemConfirm.content", {systemName: systemToDelete.name})}</p>`,
+                        yes: () => {
+                            const survivingSystems = craftingSystems.filter(system => system.id !== systemId && system.locked);
+                            GAME.settings.set(Properties.module.id, Properties.settings.craftingSystems.key, survivingSystems);
+                        }
+                    });
+                    craftingSystemManagerApp._selectedSystem = null;
+                    if (systemToDelete === this._selectedSystem) {
+                        this._selectedSystem = null;
+                    }
+                    return;
                 }
             },
             {
                 name: `${Properties.module.id}.CraftingSystemManagerApp.contextMenu.duplicate`,
                 icon: `<i class="fa-solid fa-paste"></i>`,
                 callback: async (element: JQuery) => {
-                    console.log(element.data()["systemId"]);
+                    const systemId = element.data()["systemId"] as string;
+                    const GAME = new GameProvider().globalGameObject();
+                    const craftingSystems = GAME.settings.get(Properties.module.id, Properties.settings.craftingSystems.key) as CraftingSystemDefinition[];
+
+                    const systemToDuplicate = craftingSystems.find(system => system.id === systemId);
+                    const duplicatedSystem = foundry.utils.deepClone(systemToDuplicate);
+                    duplicatedSystem.id = randomID();
+                    duplicatedSystem.name = `${duplicatedSystem.name} (copy)`
+                    duplicatedSystem.locked = false;
+
+                    craftingSystems.push(duplicatedSystem);
+                    await GAME.settings.set(Properties.module.id, Properties.settings.craftingSystems.key, craftingSystems);
+                    return;
                 }
             }
         ]);
