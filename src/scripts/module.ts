90df8b16e974b91e2c5ea3a54d44708a355e7e3f
Matt Potts <matt@mrpotts.uk>
2024-02-24 17:49:26 +0000

    Fix: Emdedded system not installed (#289)
    Co-authored-by: Matt Potts <matt@mrpotts.uk>

--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -102,6 +102,8 @@ Hooks.once(`${Properties.module.id}.ready`, async (fabricateAPI: FabricateAPI) =
         await fabricateAPI.migration.migrateAll();
     }
 
+    await fabricateAPI.migration.restoreEmbeddedCraftingSystems(true);
+
 });
 
 Hooks.on("deleteItem", async (item: any) => {
